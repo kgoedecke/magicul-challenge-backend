27149f0bec1094738c7abfb56b435269feba55c8
Philipe <philipep.galdino@gmail.com>
2021-12-05 19:25:57 -0300

    update: Added the whole setup for TypeORM, Prettier, ESLint & Typescript

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+import routes from './routes'
+
+export const app = express()
+app.use(cors())
+app.use(helmet())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(morgan('combined'))
+app.use('/', routes)
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
---
+++ src/types/express/index.d.ts
@@ -0,0 +1,5 @@
+declare namespace Express {
+  export interface Response {
+    customSuccess(httpStatusCode: number, message: string, data?: any): Response
+  }
+}
