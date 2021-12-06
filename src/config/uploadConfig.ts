8c9215298b9e4e62d12ac06ce8a481b1a0df689c
Philipe <philipep.galdino@gmail.com>
2021-12-06 17:58:43 -0300

    feat: Added set up for using Multer as middleware for files uploading

---
+++ src/config/uploadConfig.ts
@@ -0,0 +1,17 @@
+import crypto from 'crypto'
+import path from 'path'
+
+import multer from 'multer'
+
+export default {
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', 'files'),
+    filename(request, file, callback) {
+      const hash = crypto.randomBytes(6).toString('hex')
+
+      const fileName = `${hash}-${file.originalname}`
+
+      callback(null, fileName)
+    },
+  }),
+}
