1bd6d316503ea23b64f9a36b89b2f5a644a9ea37
Philipe <philipep.galdino@gmail.com>
2021-12-08 16:57:34 -0300

    update: Changed to get user_id from query params

--- src/controllers/file/list.ts
+++ src/controllers/file/list.ts
@@ -5,9 +5,9 @@ import { File } from '../../typeorm/entities/File'
 import { User } from '../../typeorm/entities/User'
 
 export const list = async (request: Request, response: Response) => {
-  const { user_id } = request.body
+  const { user_id } = request.query
 
-  const user = await User.findById(user_id)
+  const user = await User.findById(parseInt(user_id as string))
   if (!user) return response.status(404).send('User not found')
 
   const userFiles = await getRepository(File).find({ where: { owner: user } })
